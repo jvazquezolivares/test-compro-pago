4b594901c9cc13dd24bc910699df635cab72a8aa
Javier Vazquez <jvazquezolivares@gmail.com>
2017-01-13 18:13:31 -0600

    se anadio resources para contactos

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
      namespace :v1 do
        resources :usuarios
+       resources :contactos
      end
    end
   # The priority is based upon order of creation: first created -> highest priority.
